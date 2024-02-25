a48e1cef15e9d35e7bd7aba698c6b7e1e1a282b6
Stephanie Becerra <stephanie.becerra.2018@gmail.com>
2024-02-25 15:50:50 -0800

    API Attempt

--- script/app.js
+++ script/app.js
@@ -1,22 +1,36 @@
 const ApiController = (function(){
 
-const client_id = '5e097fed8bb4473490fa0922390d65c6' ;
-const client_credentials = '73aa230682e249c2aac2e905519aef86' ; 
+const clientId = '5e097fed8bb4473490fa0922390d65c6' ;
+const clientSecret = '73aa230682e249c2aac2e905519aef86' ; 
 
-async function getToken() {
-    const response = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      body: new URLSearchParams({
-        'grant_type': 'client_credentials',
-      }),
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
-      },
+const _getToken = async () => {
+    const result = await fetch ('https://accounts.spotify.com/api/token', {
+    method: "POST",
+    headers:{
+        "Content-Type": 'application/x-www-form-urlencoded',
+        "Authorization":"Basic " +  btoa(clientId+':'+ clientSecret)
+        } ,
+        body : 'grant_type=client_credentials'
     });
-  
-    const data = await response.json();
+
+    const data = await result.json();
     return data.access_token;
-  }
+}
+
+const _getRaveGenres = async (token) =>{
+    const result ('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
+        method: 'GET', 
+        headers: {'Authorization':'Bearer '+ token}
+    });
+
+    const data = await result.json();
+    const items = data.categories.items;
+
+    const desiredCategories = ['techno', 'detroit-techno', 'electro', 'eletronic'];
+    const filteredItems = items.filter(item => desiredCategories.includes(item.id));
+
+    return filteredItems;
+}
+
 })();
 
